ba5100c14fdb366d112d98388fe88e86052fac93
perak <petar.korponaic@gmail.com>
2014-05-10 14:56:05 +0200

    Now working in devel mode: in regular browser (cordova serve)

--- fusion.js
+++ fusion.js
@@ -1,6 +1,9 @@
 var connectHandlers = WebApp.connectHandlers;
 
-getPayload = function(hostName, usingAppCache) {
+var FUSION         = 0;
+var FUSION_CORDOVA = 1;
+
+getPayload = function(hostName, method) {
 	var jsFiles = [];
 	var cssFiles = [];
 	var rootUrl = Meteor.absoluteUrl("", { rootUrl: hostName });
@@ -21,19 +24,25 @@ getPayload = function(hostName, usingAppCache) {
 		}
 	});
 
-	if(usingAppCache) {
-		// client doesn't use appcache - scripts are stored cache
+	var __meteor_fusion__ = {
+		js: jsFiles,
+		css: cssFiles
+	}
+
+	// regular script loading - uses appcache if device is offline
+	if(method == FUSION) {
+		// client use appcache - scripts are stored cached
 		return "(function(){"
 		+" window.history.replaceState( {} , '', '"+rootUrl+"' );"
-		+"  var __meteor_fusion__ = {"
-		+"    js: " + JSON.stringify(jsFiles)
-		+"    , css: " + JSON.stringify(cssFiles)
-		+"  };"
+		+"  __meteor_runtime_config__ = " + JSON.stringify(runTimeConfig) + ";"
+		+" var __meteor_fusion__ = " + JSON.stringify(__meteor_fusion__) + ";"
+		+""
 		+""
 		+"  function addScript(src, loadCb, errorCb){"
 		+"    var script = document.createElement('script');"
 		+"    script.type = 'text/javascript';"
 		+"    script.src = src;"
+		+"    script.async = false;"
 		+"    script.onload = loadCb;"
 		+"    script.onerror = errorCb;"
 		+""
@@ -54,8 +63,6 @@ getPayload = function(hostName, usingAppCache) {
 		+"    addLink(__meteor_fusion__.css[i]);"
 		+"  }"
 		+""
-		+"  __meteor_runtime_config__ = " + JSON.stringify(runTimeConfig) + ";"
-		+""
 		+"  for(var i = 0; i < __meteor_fusion__.js.length - 1; i++){"
 		+"    addScript(__meteor_fusion__.js[i]);"
 		+"  }"
@@ -67,86 +74,85 @@ getPayload = function(hostName, usingAppCache) {
 		+"      document.location.reload();"
 		+"  });"
 		+"})()";
-	} else {
+	}
+
+	// loads scripts from localStorage (TODO: use cordova file system instead of localStorage!!!)
+	if(method == FUSION_CORDOVA) {
 		// client doesn't use appcache - scripts are stored in localStorage
 		return "(function() {"
-		+"\r\nwindow.history.pushState({}, '', '" + rootUrl + "');"
-		+"\r\nvar __meteor_fusion__ = {"
-		+"\r\n  js: " + JSON.stringify(jsFiles) + ","
-		+"\r\n  css: " + JSON.stringify(cssFiles)
-		+"\r\n};"
-		+"\r\n"
-		+"\r\nfunction hashCode(str) {"
-		+"\r\n	var hash = 0;"
-		+"\r\n	if (str.length == 0) return hash;"
-		+"\r\n	for (i = 0; i < str.length; i++) {"
-		+"\r\n		char = str.charCodeAt(i);"
-		+"\r\n		hash = ((hash << 5) - hash) + char;"
-		+"\r\n		hash = hash & hash;"
-		+"\r\n	}"
-		+"\r\n	return (hash >>> 0).toString(36);"
-		+"\r\n}"
+		+"\r\n window.history.replaceState({}, '', '" + rootUrl + "');"
+		+"\r\n __meteor_runtime_config__ = " + JSON.stringify(runTimeConfig) + ";"
+		+"\r\n var __meteor_fusion__ = " + JSON.stringify(__meteor_fusion__) + ";"
+		+"\r\n "
+		+"\r\n function hashCode(str) {"
+		+"\r\n 	var hash = 0;"
+		+"\r\n 	if (str.length == 0) return hash;"
+		+"\r\n 	for (i = 0; i < str.length; i++) {"
+		+"\r\n 		char = str.charCodeAt(i);"
+		+"\r\n 		hash = ((hash << 5) - hash) + char;"
+		+"\r\n 		hash = hash & hash;"
+		+"\r\n 	}"
+		+"\r\n 	return (hash >>> 0).toString(36);"
+		+"\r\n }"
 
-		+"\r\nfunction addLink(src) {"
-		+"\r\n	var link = document.createElement('link');"
-		+"\r\n	link.rel = 'stylesheet';"
-		+"\r\n	link.innerHTML = src;"
-		+"\r\n"
-		+"\r\n	var head = document.getElementsByTagName('head')[0];"
-		+"\r\n	if(head) {"
-		+"\r\n		head.appendChild(link);"
-		+"\r\n	} else {"
-		+"\r\n		document.appendChild(link);"
-		+"\r\n	}"
-		+"\r\n}"
+		+"\r\n function addLink(src) {"
+		+"\r\n 	var link = document.createElement('link');"
+		+"\r\n 	link.rel = 'stylesheet';"
+		+"\r\n 	link.innerHTML = src;"
+		+"\r\n "
+		+"\r\n 	var head = document.getElementsByTagName('head')[0];"
+		+"\r\n 	if(head) {"
+		+"\r\n 		head.appendChild(link);"
+		+"\r\n 	} else {"
+		+"\r\n 		document.appendChild(link);"
+		+"\r\n 	}"
+		+"\r\n }"
 
-		+"\r\nfunction addScript(src) {"
-		+"\r\n	var script = document.createElement('script');"
-		+"\r\n	script.type = 'text/javascript';"
-		+"\r\n	script.innerHTML = src;"
-		+"\r\n	var head = document.getElementsByTagName('head')[0];"
-		+"\r\n	if(head) {"
-		+"\r\n		head.appendChild(script);"
-		+"\r\n	} else {"
-		+"\r\n		document.appendChild(script);"
-		+"\r\n	}"
-		+"\r\n}"
+		+"\r\n function addScript(src) {"
+		+"\r\n 	var script = document.createElement('script');"
+		+"\r\n 	script.type = 'text/javascript';"
+		+"\r\n 	script.innerHTML = src;"
+		+"\r\n 	var head = document.getElementsByTagName('head')[0];"
+		+"\r\n 	if(head) {"
+		+"\r\n 		head.appendChild(script);"
+		+"\r\n 	} else {"
+		+"\r\n 		document.appendChild(script);"
+		+"\r\n 	}"
+		+"\r\n }"
 
-		+"\r\nfunction addFiles(fileList, typeOfFiles) {"
-		+"\r\n	fileList.map(function(fileURL) {"
-		+"\r\n	  var hash = hashCode(fileURL);"
+		+"\r\n function addFiles(fileList, typeOfFiles) {"
+		+"\r\n 	fileList.map(function(fileURL) {"
+		+"\r\n 	  var hash = hashCode(fileURL);"
+		+"\r\n 	  var fileContent = window.localStorage.getItem(hash);"
+		+"\r\n 	  if(!fileContent) {"
+		+"\r\n 		var request = new XMLHttpRequest();"
+		+"\r\n 		request.open('GET', fileURL, false);"
+		+"\r\n 		request.send(null);"
 
-		+"\r\n	  var fileContent = window.localStorage.getItem(hash);"
-		+"\r\n	  if(!fileContent) {"
-		+"\r\n		var request = new XMLHttpRequest();"
-		+"\r\n		request.open('GET', fileURL, false);"
-		+"\r\n		request.send(null);"
+		+"\r\n 		if (request.status === 200) {"
+		+"\r\n 			fileContent = request.responseText;"
+		+"\r\n 			window.localStorage.setItem(hash, fileContent);"
+		+"\r\n 		} else {"
+		+"\r\n 			console.log('Error ' + request.status + ' ' + request.statusText);"
+		+"\r\n 		}"
+		+"\r\n 	  }"
 
-		+"\r\n		if (request.status === 200) {"
-		+"\r\n			fileContent = request.responseText;"
-		+"\r\n			window.localStorage.setItem(hash, fileContent);"
-		+"\r\n		} else {"
-		+"\r\n			console.log('Error ' + request.status + ' ' + request.statusText);"
-		+"\r\n		}"
-		+"\r\n	  }"
-
-		+"\r\n	  if(fileContent) {"
-		+"\r\n		if(typeOfFiles == 'css') addLink(fileContent);"
-		+"\r\n		if(typeOfFiles == 'js') addScript(fileContent);"
-		+"\r\n	  }"
-		+"\r\n  });"
-		+"\r\n}"
-		+"\r\n__meteor_runtime_config__ = " + JSON.stringify(runTimeConfig) + ";"
-		+"\r\naddFiles(__meteor_fusion__.css, 'css');"
-		+"\r\naddFiles(__meteor_fusion__.js, 'js');"
-		+"\r\naddScript("
-		+"\r\n	  'if (typeof Package === \"undefined\" || '"
-		+"\r\n	+ '        ! Package.webapp || '"
-		+"\r\n	+ '        ! Package.webapp.WebApp || '"
-		+"\r\n	+ '        ! Package.webapp.WebApp._isCssLoaded()) '"
-		+"\r\n	+ '  document.location.reload();'"
-		+"\r\n);"
-		+"\r\n})()";
+		+"\r\n 	  if(fileContent) {"
+		+"\r\n 		if(typeOfFiles == 'css') addLink(fileContent);"
+		+"\r\n 		if(typeOfFiles == 'js') addScript(fileContent);"
+		+"\r\n 	  }"
+		+"\r\n   });"
+		+"\r\n }"
+		+"\r\n addFiles(__meteor_fusion__.css, 'css');"
+		+"\r\n addFiles(__meteor_fusion__.js, 'js');"
+		+"\r\n addScript("
+		+"\r\n 	  'if (typeof Package === \"undefined\" || '"
+		+"\r\n 	+ '        ! Package.webapp || '"
+		+"\r\n 	+ '        ! Package.webapp.WebApp || '"
+		+"\r\n 	+ '        ! Package.webapp.WebApp._isCssLoaded()) '"
+		+"\r\n 	+ '  document.location.reload();'"
+		+"\r\n );"
+		+"\r\n })()";
 	}
 }
 
@@ -155,11 +161,11 @@ connectHandlers.use(handleFusionRequest);
 function handleFusionRequest(req, res, next) {
 	if(req.url.match('/fusion$')) {
 		res.setHeader("Content-Type", "application/javascript");
-		res.end(getPayload(req.headers.host, true));
+		res.end(getPayload(req.headers.host, FUSION));
 	} else {
-		if(req.url.match('/fusion-no-appcache$')) {
+		if(req.url.match('/fusion-cordova$')) {
 			res.setHeader("Content-Type", "application/javascript");
-			res.end(getPayload(req.headers.host, false));
+			res.end(getPayload(req.headers.host, FUSION_CORDOVA));
 		} else {
 			return next();
 		}
